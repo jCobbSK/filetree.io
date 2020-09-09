459de668ceff8f62ab472b0edaed8cb320e1e14b
Jakub Kanitra <>
2020-09-09 18:29:31 +0200

    Add cypress scaffold and setup

---
+++ cypress/integration/app.spec.js
@@ -0,0 +1,7 @@
+describe('Landing page', () => {
+    it('should open default application', () => {
+        cy.visit('/');
+
+        cy.get('[data-cy=application-wrapper]').should('be.visible');
+    })
+})
